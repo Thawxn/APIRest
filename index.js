eba8ba69c695da7db453a6741b0e07de6f90fc53
Thawan Marques <thawan28032001@gmail.com>
2022-12-14 01:06:53 -0300

    APIRest commit project

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ connection
     });
 
 
+//Rota listagem de Games
 app.get('/games', (req, res) => {
     Game.findAll().then(games => {
         res.statusCode = 200
@@ -24,25 +25,55 @@ app.get('/games', (req, res) => {
     })
 });
 
+//Rota listagem de um game pelo id
 app.get('/game/:id', (req, res) => {
     let id = req.params.id;
 
     if(isNaN(id)){
         res.sendStatus(400)
     }else{
-
         Game.findOne({raw: true, where: {id}}).then(games => {
             if(games == null){
                 res.sendStatus(404)
             }else{
-                
+                res.statusCode = 200
                 res.json(games)
             }
         })
-
     }
+});
 
-})
+//Rota para adicionar um game
+app.post('/game', (req, res) => {
+    let title = req.body.title;
+    let year = req.body.year;
+    let price = req.body.price;
+
+    if(title == undefined || year == undefined || price == undefined){
+        res.sendStatus(400)
+    }else{
+        Game.create({
+            title: title,
+            year: year,
+            price: price
+        }).then(() => {
+            res.sendStatus(200)
+        })
+    }
+});
+
+//Rota para deletar um game
+app.delete('/game/:id', (req, res) => {
+    let id = req.params.id;
+
+    if(isNaN(id)){
+        res.sendStatus(400)
+    }else{
+        Game.destroy({where: {id}}).then(() => {
+            res.sendStatus(200)
+        })
+    }
+});
 
 
 app.listen(3000, () => {
